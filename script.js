70504af1b4355cf810822f6813ab777ec5b815dd
jyliao369 <jyliao369@gmail.com>
2021-06-20 15:52:10 -0400

    Added a boolean variable as a method to turn off or on special characters and numbers

--- script.js
+++ script.js
@@ -9,15 +9,25 @@ var char = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
 var num = [0,1,2,3,4,5,6,7,8,9];
 var specChar = ["~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","|","<",",",">",".","?","/"];
 // This will randomize which char gets selected and added to the overall password
-// 0 = char, 1 = num, 2 = specChar, and 3 = upper case letter
+// 0 = upper case char, 1 = num, 2 = specChar, and 4 = just char
 var charSel = [0,1,2,3];
 // How long you want the pass word to be.
 var passlength = 15;
 // An empty string to create your password.
 var pass = "";
+// These variables are booleans. They are used in the future where the site prompts the user if they
+// would like to use certain characters in their passwords like numbers, special characters, or upper case specifically
+var numOn = true;
+var specCharOn = true;
+var charUpOn = true;
 
 
 var genPass = function() {
+
+  // this is to test to see if the say the special characters are turned off or don't want to be used
+  // specCharOn = false;
+  // numOn = false;
+  // charUpOn = false;
 
   for (var a = 0; a < passlength; a++) {
 
@@ -31,14 +41,14 @@ var genPass = function() {
     var ranCharSel = Math.floor(Math.random() * charSel.length);
 
     // This will determine which character will be added to the overall password
-    if (ranCharSel === 0) {
-      pass += char[ranChar];
-    } else if (ranCharSel === 1) {
+    if (ranCharSel === 0 && charUpOn) {
+      pass += char[ranChar].toUpperCase();;
+    } else if (ranCharSel === 1 && numOn) {
       pass += num[ranNum];
-    } else if (ranCharSel === 2) {
+    } else if (ranCharSel === 2 && specCharOn) {
       pass += specChar[ranSpecChar];
     } else {
-      pass += char[ranChar].toUpperCase();
+      pass += char[ranChar];
     }
   }
 
